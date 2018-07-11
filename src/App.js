eb03fae7673024634cb890a5a28a9c1bac2ad62b
Dhayanandhan R <dhaya.r@partner.samsung.com>
2018-07-11 18:00:30 +0530

    add more queries

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ const host = 'http://localhost:4000/v1';
 
 class App extends Component {
   state = {
-    quote: ''
+    quote: '',
+    random: 0,
+    rollDice: [0, 0, 0],
+    login: false
   };
 
   componentDidMount = async () => {
@@ -15,6 +18,8 @@ class App extends Component {
     const query = `{
       hello
       quoteOfTheDay
+      random
+      rollDice(numDice:4,numSides:4)
     }`;
     const response = await fetch(host, {
       method: 'POST',
@@ -28,15 +33,44 @@ class App extends Component {
       })
     }).then(data => data.json());
     const {
-      data: { quoteOfTheDay }
+      data: { quoteOfTheDay, random, rollDice }
     } = response;
     this.setState({
-      quote: quoteOfTheDay
+      quote: quoteOfTheDay,
+      random,
+      rollDice
+    });
+  };
+
+  loginUser = () => {
+    const username = 'dhaya';
+    const password = '123456';
+    const query = `mutation {
+      login(username: $username, password: $password)
+    }`;
+
+    const response = fetch(host, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        query,
+        variables: { username, password }
+      })
+    }).then(data => data.json());
+
+    const {
+      data: { login }
+    } = response;
+    this.setState({
+      login
     });
   };
 
   render() {
-    const { quote } = this.state;
+    const { quote, random, rollDice, login } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -51,6 +85,36 @@ Quote of the day
         <h2>
           {quote}
         </h2>
+        <h3>
+This is a random number
+        </h3>
+        <h2>
+          {random}
+        </h2>
+        <h5>
+Dice Scores
+        </h5>
+        {rollDice.map((dice, i) => (
+          <p key={i}>
+            {dice}
+          </p>
+        ))}
+
+        <h1>
+Login
+        </h1>
+        <button onClick={this.loginUser}>
+Login User
+        </button>
+        {login ? (
+          <h1>
+Logged in
+          </h1>
+        ) : (
+          <h1>
+Not logged in
+          </h1>
+        )}
       </div>
     );
   }
